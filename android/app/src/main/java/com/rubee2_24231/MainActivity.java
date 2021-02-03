16d61666632a3269f24934f291e42fd8a1372cfc
Crowdbotics <team@crowdbotics.com>
2021-02-03 04:32:18 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/rubee2_24231/MainActivity.java
@@ -0,0 +1,15 @@
+package com.rubee2_24231;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "rubee2_24231";
+    }
+}
